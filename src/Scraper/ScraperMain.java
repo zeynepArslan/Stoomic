0ec4a406a0b66c3cbbb3ce5f01bee81448a5ff05
Seda Nur Doganay <seda.doganay@ozu.edu.tr>
2016-05-15 23:59:53 +0300

    -

--- src/Scraper/ScraperMain.java
+++ src/Scraper/ScraperMain.java
@@ -4,6 +4,6 @@ public class ScraperMain {
 
 	public static void main(String [ ] args)
 	{
-	    new OzyeginWebScraper();		
+	    new OzyeginWebScraper("http://www.ozyegin.edu.tr/en/AKADEMIK-PROGRAMLAR/isletme-fakultesi/People?page=1");		
 	}
 }
