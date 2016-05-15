21c9c2f0c637059682fe468a5de4d553c5360a5c
Seda Nur Doganay <seda.doganay@ozu.edu.tr>
2016-05-15 23:55:05 +0300

    scraper works

---
+++ src/Scraper/ScraperMain.java
@@ -0,0 +1,9 @@
+package Scraper;
+
+public class ScraperMain {
+
+	public static void main(String [ ] args)
+	{
+	    new OzyeginWebScraper();		
+	}
+}
--- src/Scraper/WebScraper.java
+++
@@ -1,5 +0,0 @@
-package Scraper;
-
-public class WebScraper {
-
-}
